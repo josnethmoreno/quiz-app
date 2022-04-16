0264896e77cdd4facb6ebd11590d7b1d99ee40b9
josnethmoreno <josnethmoreno@gmail.com>
2022-04-16 10:45:11 -0400

    Update message score

--- models/UI.js
+++ models/UI.js
@@ -18,11 +18,33 @@ export class UI {
 		});
 	}
 
-	showScore(score){
+	showScore(choicesLength, score){
 		const choiceDiv = document.getElementById('choices');
 		const question = document.getElementById('question');
+		let messageScore = '';
+
+		if(score === 0) messageScore = 'Oh no! No has logrado acertar ninguna respuesta!';
+		if(score !== 0) messageScore = `Wow! has logrado acertar ${score} respuestas de ${choicesLength} preguntas`;
+		if(score === choicesLength) messageScore = 'WTFFF! ¿Eres de la nasa? Acertaste todas las respuesta:O';
+
 		question.innerText = `Tu puntuación ha sido`;
-		choiceDiv.innerHTML = `${score}`;
+		choiceDiv.innerHTML = `
+			<div class="choices-score p-2 d-flex flex-column justify-content-center align-items-center w-75 h-75 mx-auto my-auto rounded bg-primary bg-opacity-10">
+        <div class="choice-score-message h5 text-center">
+          ${messageScore}
+        </div>
+        <div class="choice-score-actions mt-5 text-center d-flex flex-column flex-md-row gap-2">
+          <button id="btnAcierto" class="btn btn-outline-warning">Ver aciertos!</button>
+          <button id="btnReset" class="btn btn-outline-primary">Intentar de nuevo!</button>
+          <button id="btnMessage" class="btn btn-danger">Mensaje oculto</button>
+        </div>
+      </div>
+		`;
+
+		const btnAcierto = document.getElementById('btnAcierto');
+		btnAcierto.addEventListener('click', () => {
+			location.reload();
+		})
 	}
 
 	updateProgress(questionIndex, questionsLength){
